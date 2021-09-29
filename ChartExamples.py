b65a3ea93c251dee9fe390d5f45e5e4196837a13
evksin <73026357+evksin@users.noreply.github.com>
2021-09-29 21:43:26 +0300

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,18 @@
+#Примеры построения графиков
+
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText as st
+from tkinter import messagebox as mb
+from tkinter import filedialog as fd
+import os
+import pandas as pd
+
+#Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Примеры построения графиков")
+
+#Запуск цикла mainloop
+window.mainloop()
+
+
