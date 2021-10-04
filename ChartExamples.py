e08787092a0f3bb2239d7af2b3e4dd1234bc2081
evksin <73026357+evksin@users.noreply.github.com>
2021-10-04 22:03:06 +0300

    Добавление групп кнопка - метка

--- ChartExamples.py
+++ ChartExamples.py
@@ -13,7 +13,7 @@ def do_close():
 
 #Создание главного окна
 window = tk.Tk()
-window.geometry("450x450")
+window.geometry("450x550")
 window.title("Примеры построения графиков")
 
 #Добавление метки заголовка
@@ -35,15 +35,43 @@ lblChart2 = tk.Label(text='Нормальное распределение')
 lblChart2.place(x=170, y=168)
 
 #Добавление кнопки и метки для графика 3
-btnChart2 = tk.Button(window, text='График 3', font = ('Helvetica', 10, 'bold'), command=chart3.plot_chart)
-btnChart2.place(x=40, y=215, width=90, height=30)
+btnChart3 = tk.Button(window, text='График 3', font = ('Helvetica', 10, 'bold'), command=chart3.plot_chart)
+btnChart3.place(x=40, y=215, width=90, height=30)
 
-lblChart2 = tk.Label(text='3D поверхность')
-lblChart2.place(x=170, y=220)
+lblChart3 = tk.Label(text='3D поверхность')
+lblChart3.place(x=170, y=220)
+
+#Добавление кнопки и метки для графика 4
+btnChart2 = tk.Button(window, text='График 4', font = ('Helvetica', 10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=265, width=90, height=30)
+
+lblChart2 = tk.Label(text='Нормальное распределение - 3 графика')
+lblChart2.place(x=170, y=268)
+
+#Добавление кнопки и метки для графика 5
+btnChart2 = tk.Button(window, text='График 5', font = ('Helvetica', 10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=315, width=90, height=30)
+
+lblChart2 = tk.Label(text='Нормальное распределение')
+lblChart2.place(x=170, y=320)
+
+#Добавление кнопки и метки для графика 6
+btnChart2 = tk.Button(window, text='График 6', font = ('Helvetica', 10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=365, width=90, height=30)
+
+lblChart2 = tk.Label(text='Нормальное распределение')
+lblChart2.place(x=170, y=368)
+
+#Добавление кнопки и метки для графика 7
+btnChart2 = tk.Button(window, text='График 7', font = ('Helvetica', 10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=415, width=90, height=30)
+
+lblChart2 = tk.Label(text='Нормальное распределение')
+lblChart2.place(x=170, y=420)
 
 #Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text='Закрыть', font = ('Helvetica', 10, 'bold'), command=do_close)
-btnClose.place(x=330, y=400, width=90, height=30)
+btnClose.place(x=330, y=500, width=90, height=30)
 
 #Запуск цикла mainloop
 window.mainloop()
